a847f3d30f73174f96d6789764eb417f4e1014ec
clandestine11 <metis_007@yahoo.com>
2017-01-03 19:14:17 +0200

    Add layout and movement

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+		void Update () {
+		//Rotation
+		float rotX = Input.GetAxis("Mouse X");
+		transform.Rotate (0, rotX, 0);
+		//Movement
+		float forwardSpeed = Input.GetAxis("Vertical")/2.0f;
+		float sideSpeed = Input.GetAxis("Horizontal")/2.0f;
+		Vector3 speed = new Vector3(sideSpeed,0,forwardSpeed);
+		CharacterController cc = GetComponent<CharacterController>();
+        cc.SimpleMove(speed);
+	}
+}
+ 
