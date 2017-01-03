5ab20248260f54caedb69618ec681d9efa1977e2
clandestine11 <metis_007@yahoo.com>
2017-01-03 21:20:56 +0200

    Add broccoli and bullet (not working)

---
+++ Assets/scripts/shooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class shooting : MonoBehaviour {
+
+	public Rigidbody bullet_prefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Camera cam = GameObject.Find ("CameraPlayer").GetComponent<Camera> ();
+
+			Rigidbody theBullet;
+			theBullet = Instantiate (bullet_prefab, cam.transform.position, cam.transform.rotation) as Rigidbody ;
+			theBullet.AddForce (cam.transform.forward*5000);
+		}	
+	}
+}
