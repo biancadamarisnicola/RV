7504e356efb25821ab6c29d5461b8976eb4f67e6
clandestine11 <metis_007@yahoo.com>
2017-01-03 20:28:12 +0200

    Add jumping

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -2,18 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(CharacterController))]
 public class playerController : MonoBehaviour {
 
 	public float upDownRange = 30.0f;
-	float rotY = 0;
+	public float rotY = 0;
+	public float verticalVelocity =0;
+	public float jumpSpeed = 20.0f;
+	CharacterController cc;
 
 	// Use this for initialization
 	void Start () {
 		Screen.lockCursor = true;
+		cc = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
-		void Update () {
+	void Update () {
+		CharacterController cc = GetComponent<CharacterController>();
+
 		//Rotation
 		float rotX = Input.GetAxis("Mouse X");
 		transform.Rotate (0, rotX, 0);
@@ -22,14 +29,22 @@ public class playerController : MonoBehaviour {
 		rotY = Mathf.Clamp (rotY, -upDownRange, upDownRange);
 		GameObject.Find ("CameraPlayer").GetComponent<Camera>().transform.localRotation = Quaternion.Euler (rotY, 0, 0);
 
-
 		//Movement
 		float forwardSpeed = Input.GetAxis("Vertical")/1.5f;
 		float sideSpeed = Input.GetAxis("Horizontal")/1.5f;
-		Vector3 speed = new Vector3(sideSpeed,0,forwardSpeed);
+
+		verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+		//cc.isGrounded - to check if out player is at the ground
+
+		if ( Input.GetButtonDown ("Jump")) {
+			verticalVelocity = 5.0f;
+		};
+			
+		Vector3 speed = new Vector3(sideSpeed,verticalVelocity,forwardSpeed);
 		speed = transform.rotation * speed;
-		CharacterController cc = GetComponent<CharacterController>();
-        cc.SimpleMove(speed);
+
+		cc.Move(speed*Time.deltaTime);
 	}
 }
  
